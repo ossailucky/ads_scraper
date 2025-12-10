9434c457cdf7677b43772c950ae824b035ca3631
ossailucky <luckyblaqy@gmail.com>
2025-12-10 19:47:54 +0100

    change

--- examples/basic-usage.ts
+++ examples/basic-usage.ts
@@ -3,7 +3,6 @@ import { SyncManager } from '../src';
 async function basicExample() {
   const syncManager = new SyncManager();
   
-  // Initial sync
   const url = 'https://www.facebook.com/ads/library/?active_status=active&ad_type=all&country=ALL&is_targeted_country=false&media_type=all&search_type=page&view_all_page_id=282592881929497';
   const result = await syncManager.initialSync(url, 50);
   
